743dc257da0d584d82d1b3b8ddc72cd701b61857
pjesus140 <pierre.jesus@academytrainee.com>
2019-07-19 11:30:33 +0100

    UserService added ver1

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,34 @@
+package service;
+
+import javax.inject.Inject;
+
+import persistence.repo.UserRepo;
+
+public class UserService {
+	
+	@Inject
+	private UserRepo repo;
+	
+	public String createUser(String user) {
+		
+		return this.repo.createUser(user);
+	}
+	
+	public String CheckLogIn(String user) {
+		return this.repo.CheckLogIn(user);
+	}
+	
+	public String updateUsers(long userId, String user) {
+		
+		return this.repo.updateUsers(userId, user);
+		
+	}
+	
+	public String deleteUser(long userId) {
+		return this.repo.deleteUser(userId);
+	}
+	
+	
+	
+
+}
