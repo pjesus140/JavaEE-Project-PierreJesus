636b640e9bb34fb418f50622ecad192e972b0479
pjesus140 <pierre.jesus@academytrainee.com>
2019-07-19 11:08:30 +0100

    This adds the config and Persistence layer for User table

---
+++ src/main/java/persistence/repo/UserRepo.java
@@ -0,0 +1,48 @@
+package persistence.repo;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+
+import persistence.domain.User;
+import util.JSONUtil;
+
+@Transactional(value = TxType.REQUIRED)
+public class UserRepo {
+	
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+	
+	@Inject
+	private JSONUtil gson;
+	
+	public String createUser(String user) {
+		this.manager.persist(this.gson.getObjectForJSON(user, User.class));
+		return "Success:"+user;
+	}
+	
+	@Transactional(value = TxType.SUPPORTS)
+	public String getAllUsers() {
+		TypedQuery<User> query = this.manager.createQuery("SELECT m FROM Movie m", User.class);
+		return this.gson.getJSONForObject(query.getResultList());
+	}
+	
+	public String updateUsers(long userId, String user) {
+		User current = this.manager.find(User.class, userId);
+		User newUser = this.gson.getObjectForJSON(user, User.class);
+		current.setUsername(newUser.getUsername());
+		this.manager.persist(current);
+		return "Success";
+	}
+	
+	public String deleteUser(long userId) {
+		this.manager.remove(this.manager.find(User.class, userId));
+		return "Success:"+this.manager.find(User.class, userId);
+	}
+	
+
+}
