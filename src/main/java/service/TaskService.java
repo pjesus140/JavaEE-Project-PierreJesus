d5dc4c95c5c0f7e0dd52e859b777aa355993bf3e
pjesus140 <pierre.jesus@academytrainee.com>
2019-07-31 16:47:54 +0100

    tasks can't be null

--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -2,16 +2,24 @@ package service;
 
 import javax.inject.Inject;
 
-
+import persistence.domain.ListsTodo;
+import persistence.domain.Tasks;
 import persistence.repo.TaskRepo;
+import util.JSONUtil;
 
 public class TaskService {
 
+	@Inject
+	private JSONUtil gson;
 	
 	@Inject
 	private TaskRepo repo;
 	
 	public String CreateTask(Long listId, String taskText) {
+		Tasks aTask = this.gson.getObjectForJSON(taskText, Tasks.class);
+		if(aTask.getTaskText().isEmpty()) {
+			return "{\"Success\":\"False\"}";
+		}
 		
 		return this.repo.CreateTask(listId,taskText);
 		
